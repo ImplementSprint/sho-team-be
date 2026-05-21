aef0231eccf4088d6c1f9be5cfe9271bf4cc56d9
shmce <shoti12142004@gmail.com>
2026-05-22 06:45:33 +0800

    push

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,4 @@
-module.exports = {
+const baseConfig = {
   displayName: 'sho-team-be',
   moduleFileExtensions: ['js', 'json', 'ts'],
   rootDir: '.',
@@ -14,3 +14,50 @@ module.exports = {
   coverageDirectory: 'coverage',
   testEnvironment: 'node',
 };
+
+const ciProjectAliases = [
+  'sho-team-be',
+  'api',
+  'location-service',
+  'api-gateway',
+  'servease-api-gateway',
+  'auth-service',
+  'servease-auth-service',
+  'user-service',
+  'servease-user-service',
+  'catalog-service',
+  'servease-catalog-service',
+  'booking-service',
+  'servease-booking-service',
+  'availability-service',
+  'servease-availability-service',
+  'messaging-service',
+  'servease-messaging-service',
+  'payment-service',
+  'servease-payment-service',
+  'review-service',
+  'servease-review-service',
+  'notification-service',
+  'servease-notification-service',
+  'support-service',
+  'servease-support-service',
+  'admin-service',
+  'servease-admin-service',
+];
+
+const isSelectingProjects = process.argv.some(
+  (arg) => arg === '--selectProjects' || arg.startsWith('--selectProjects='),
+);
+
+const rootConfig = { ...baseConfig };
+delete rootConfig.displayName;
+
+module.exports = isSelectingProjects
+  ? {
+      ...rootConfig,
+      projects: ciProjectAliases.map((displayName) => ({
+        ...baseConfig,
+        displayName,
+      })),
+    }
+  : baseConfig;
